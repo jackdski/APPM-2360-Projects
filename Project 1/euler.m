3c4c2540bd3708c583a74c1dfd49864de1bd2e5e
Jack Danielski <jackdski555@gmail.com>
2017-10-01 12:09:14 -0600

    Add files via upload

--- Project 1/euler.m
+++ Project 1/euler.m
@@ -1,1 +1,18 @@
+function [ ] = euler(y, h, p, r)
+% Euler's method for APPM 2360 Project 1
+% Assume t_0 is 0
+t = 0;
+yVec = [];
+tVec = [];
+while(y ~= 0)
+    tVec = [tVec t];
+    yVec = [yVec y];
+    t = t+1;
+    y = yNew(r, h, y, p);
+end 
+    
+
+
+
+end
 
