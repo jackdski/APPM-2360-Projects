219fc66c61664663325da112bebfff45e1d1d5a7
Jack Danielski <jackdski555@gmail.com>
2017-10-04 23:05:17 -0600

    Update euler.m

--- Project 1/euler.m
+++ Project 1/euler.m
@@ -1,18 +1,27 @@
-function [ ] = euler(y, h, p, r)
+function [ t ] = eulersMethod(h)
 % Euler's method for APPM 2360 Project 1
 % Assume t_0 is 0
+y = 750000;
+p = 4000;
+r = 0.05;
+
 t = 0;
-yVec = [];
-tVec = [];
-while(y ~= 0)
-    tVec = [tVec t];
-    yVec = [yVec y];
-    t = t+1;
-    y = yNew(r, h, y, p);
-end 
-    
+yVec = 1:1000;
+tVec = 1:1000;
+index = 1;
 
-
-
+while(y > 0)
+    t = t+h;
+    y = y + h*(r*h - 12*p);
+    tVec(index) = t;
+    yVec(index) = y;
+    index = index + 1;
 end
 
+figure 
+plot(tVec, yVec)
+title('Loan Estimate with H = 0.01')
+ylabel('Dollars')
+xlabel('t (years)');
+
+end
